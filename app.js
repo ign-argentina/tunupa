a2cf9ac2a1372c7aefae33919f708e71c692bdce
Franco Leonel Cristillo <104926556+FRLCR@users.noreply.github.com>
2025-04-30 12:08:26 -0300

    Integracion Swagger UI y validación OpenAPI
    - Añadidas dependencias: swagger-ui-express, yamljs, express-openapi-validator
    
    - Implementada documentación interactiva con Swagger UI

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import config from "./src/config.js";
 import routes from "./src/routes/routes.js";
 
+import pathh from 'path'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
+
 const app = express();
 const port = config.server.port;
 const path = config.server.path;
@@ -16,6 +20,12 @@ app.use(express.urlencoded({ extended: true }));  */
 app.get("/", (req, res) => {
   res.json({ info: "Geocodr API" });
 });
+
+// Mostrar Swagger UI
+const apiSpec = pathh.join(process.cwd(), 'openapi.yaml');
+const openApiDocument = YAML.load(apiSpec);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 /****** USO DE RUTAS  ******/
 app.use(`/${path}`, routes);
--- src/models/Place.js
+++ src/models/Place.js
@@ -1,18 +1,19 @@
 import BaseModel from './BaseModel.js';
 
 const IDNORMAL =
-    "SELECT (SELECT row_to_json(t) FROM (SELECT id, name, depto, pcia) AS t) AS place FROM places WHERE id = $1 ;", // params: id
+         "SELECT (SELECT row_to_json(t) FROM (SELECT id, name, depto, pcia) AS t) AS place FROM places WHERE id = $1 ;", // params: id
     IDGEOJSON =
         "SELECT 'Feature' AS type, ST_AsGeoJSON (ST_GeometryN (geom, 1))::json AS geometry, (SELECT row_to_json(t) FROM (SELECT id, name, type, aglo, gob_local, depto, pcia, code AS cod_bahra) AS t) AS properties FROM places WHERE id = $1;", // params: id
     GEOCODE =
-    "SELECT (SELECT row_to_json(t) FROM ( SELECT id, type, name, depto, pcia, rank) AS t) AS place FROM places, to_tsquery_partial(lower(unaccent($1))) AS query, ts_rank_cd(vector, query) AS rank WHERE vector @@ query ORDER BY rank DESC LIMIT $2 ;", // params: text, limit
+        "SELECT (SELECT row_to_json(t) FROM ( SELECT id, type, name, depto, pcia, rank) AS t) AS place FROM places, to_tsquery_partial(lower(unaccent($1))) AS query, ts_rank_cd(vector, query) AS rank WHERE vector @@ query ORDER BY rank DESC LIMIT $2 ;", // params: text, limit
     GEOJSONGEOCODE =
-    "SELECT 'Feature' AS type, ST_AsGeoJSON (ST_GeometryN (geom, 1))::json AS geometry, (SELECT row_to_json(t) FROM (SELECT id, name, type, aglo, gob_local, depto, pcia, code AS cod_bahra, rank) AS t) AS properties FROM places, to_tsquery_partial(lower(unaccent($1))) AS query, ts_rank_cd(vector, query) AS rank WHERE vector @@ query ORDER BY rank DESC LIMIT $2 ;", // params: text, limit
+        "SELECT 'Feature' AS type, ST_AsGeoJSON (ST_GeometryN (geom, 1))::json AS geometry, (SELECT row_to_json(t) FROM (SELECT id, name, type, aglo, gob_local, depto, pcia, code AS cod_bahra, rank) AS t) AS properties FROM places, to_tsquery_partial(lower(unaccent($1))) AS query, ts_rank_cd(vector, query) AS rank WHERE vector @@ query ORDER BY rank DESC LIMIT $2 ;", // params: text, limit
     REVERSE =
         "SELECT (SELECT row_to_json(t) FROM (SELECT id, name, depto, pcia) AS t ) AS place FROM places WHERE ST_DWithin( ST_Transform(geom, 3857), ST_Transform(ST_SetSRID(ST_Point($1, $2), 4326), 3857), $3 ) LIMIT $4 ;", // requires 4 params: lon, lat, radius, limit
     GEOJSONREVERSE =
         "SELECT 'Feature' AS type, ST_AsGeoJSON (ST_GeometryN (geom, 1))::json AS geometry, (SELECT row_to_json(t) FROM (SELECT name, type, aglo, gob_local, depto, pcia, code AS cod_bahra) AS t) AS properties FROM places WHERE ST_DWithin( ST_Transform(geom, 3857), ST_Transform(ST_SetSRID(ST_Point($1, $2), 4326), 3857), $3 ) LIMIT $4 ;", // requires 4 params: lon, lat, radius, limit
-    INTERSECTS = "SELECT row_to_json(j) FROM (SELECT array_to_string(array_agg(distinct name),', ') AS properties, geom FROM (SELECT admin_areas.name AS name, t.geom AS geom FROM admin_areas,(SELECT ST_SetSRID(ST_Point($1, $2), 4326) AS geom) t WHERE ST_Intersects(t.geom, admin_areas.geom)) x GROUP BY geom LIMIT $3) j;"; // requires lon, lat and gives an array with all the envelopes / ¿ integrate with reverse?
+    INTERSECTS =
+        "SELECT row_to_json(j) FROM (SELECT array_to_string(array_agg(distinct name),', ') AS properties, geom FROM (SELECT admin_areas.name AS name, t.geom AS geom FROM admin_areas,(SELECT ST_SetSRID(ST_Point($1, $2), 4326) AS geom) t WHERE ST_Intersects(t.geom, admin_areas.geom)) x GROUP BY geom LIMIT $3) j;"; // requires lon, lat and gives an array with all the envelopes / ¿ integrate with reverse?
 
 class Place extends BaseModel {
     static getById = async (id, format) => {
@@ -34,37 +35,37 @@ class Place extends BaseModel {
     }
 
     static getByCoordinates = async (coords) => {
-        try{
-            
+        try {
+
             let place = []
-            
+
             if (coords.format === "list") {
                 place = await super.runQuery(REVERSE, [coords.lon, coords.lat, coords.radius, coords.limit])
             } else {
                 place = await super.runQuery(GEOJSONREVERSE, [coords.lon, coords.lat, coords.radius, coords.limit])
                 place = this.#geoJsonFormatByCoordinates(place, coords.format);
             }
-            
+
             if (!place || place.length < 1) {
                 place = await super.runQuery(INTERSECTS, [coords.lon, coords.lat, coords.limit]);
             }
 
             return place;
-        } catch(error){
+        } catch (error) {
             console.log("Error en la capa de persistencia " + error.message)
-            return []            
+            return []
         }
     }
 
     static #geoJsonFormatByCoordinates = (place, format) => {
         if (format === "geojson") {
-          return {
-            type: "FeatureCollection",
-            features: place,
-          };
+            return {
+                type: "FeatureCollection",
+                features: place,
+            };
         }
         return place;
-      };
+    };
 
     // Documentacion pendiente
     static #geojsonFormatByID = (place) => {
